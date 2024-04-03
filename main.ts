30db52fd756b1937d12ee610fcbde5dbfc666f37
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-04-03 21:11:07 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,24 @@
+// Switch Statement
+let miniBreak = "Sunday";
+switch (miniBreak) {
+   case "Monday":
+      console.log("Go for a walk");
+      break;
+   case "sunday":
+      console.log("Plan a picnic with family");
+      break;
+    default:
+        console.log("Play a musical instrument");
+}
+
+let weather = "sunny";
+switch (weather) {
+    case "cold":
+        console.log("Take hot drinks and food");
+        break;
+    case "sunny":
+        console.log("Avoid sun exposure");
+        break;
+    default:
+        console.log("Wear a rain coat");
+}
